08e9ab613cce1406d25004d1fcc7b8c1b961d559
anjel <anggri@gmail.com>
2018-02-23 13:51:20 +0100

    casi

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿
+SELECT f.Numero, f.Fecha, f.UsuariosId, l.Cantidad, p.Id, p.Nombre, p.Precio, u.Nick 
+	FROM facturas f INNER JOIN lineasfactura l ON f.Id = l.FacturaId 
+					INNER JOIN productos p ON p.Id = l.ProductoId
+					INNER JOIN usuarios u ON u.Id=f.UsuariosId
+					
+	
